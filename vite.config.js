7e5afa1aa15ec7e93a800924dd7c210c35fb87fc
ohzzy90@gmail.com <ohzzy90@gmail.com>
2025-09-22 02:58:23 -0800

    /

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        home: 'index.html',
+        main: 'index.html',
         examples: 'examples.html',
         tour: 'tour.html',
         blog: 'blog.html',
